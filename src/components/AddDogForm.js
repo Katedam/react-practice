5b4f53351eb78699d357efc8ee35b39cf8fcc0bf
Kate Dameron <katerj33@gmail.com>
2019-02-26 09:40:08 -0800

    AddDogForm updating state

--- src/components/AddDogForm.js
+++ src/components/AddDogForm.js
@@ -1,31 +1,36 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 export class AddDogForm extends PureComponent {
-  state = {
-    name: '',
-    age: '',
-    weight: ''
-  }
-
-  handleChange = (event) => {
-    event.preventDefault();
+  constructor(props) {
+    super(props);
     
   }
+
   render() {
+    console.log(this.props);
+    const { name, age, weight, onChange } = this.props;
     return (
       <form>
         <label>Name 
-          <input type="text" name="name" value={name}></input>
+          <input onChange={onChange} type="text" name="name" value={name}></input>
         </label>
         <label>Age 
-          <input type="text" name="age" value={age}></input>
+          <input onChange={onChange} type="text" name="age" value={age}></input>
         </label>
         <label>Weight 
-          <input type="text" name="weight" value={weight}></input>
+          <input onChange={onChange} type="text" name="weight" value={weight}></input>
         </label>
         <button type="submit">Add Dog</button>
       </form>
     );
   }
 }
+
+AddDogForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.string.isRequired,
+  weight: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
 
--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,27 @@ import { AddDogForm } from './AddDogForm';
 
 export default class App extends Component {
 
+  state = {
+    name: 'spot',
+    age: '2',
+    weight: '40lbs'
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
+    const { name, age, weight } = this.state;
     return (
       <>
       <h1>Add a Dog</h1>
-      <AddDogForm />
+      <AddDogForm 
+        name={name}
+        age={age}
+        weight={weight}
+        onChange={this.handleChange}
+      />
       </>
     );
   }
